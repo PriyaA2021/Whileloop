1ec436b93e9d9d81c9692b76930ad1995631f697
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-11 16:23:06 +1200

    Add project files.

---
+++ Whileloop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0, n;
+            Console.WriteLine("Enter a number until which you want the numbers to be displayed!");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(i<=n)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
